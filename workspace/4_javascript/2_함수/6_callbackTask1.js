6f1ecdbcafd9c5848a165da8a773e303ab3e2e47
JiWoo028 <cluypkoj184@gmail.com>
2024-05-13 23:38:50 +0900

    5월 8일 이벤트 실습 중

--- workspace/4_javascript/2_함수/6_callbackTask1.js
+++ workspace/4_javascript/2_함수/6_callbackTask1.js
@@ -3,13 +3,20 @@
 // 콜백 2 : 1 ~ 숫자 까지의 합을 받아오기.
 // 객체의 키값은 마음대로 해도 된다.
 
+// 숫자를 하나 전달하면
 function test(num, callback1, callback2){
+    // 짝홀 여부
     let type = callback1(num);
+
+    // 해당 숫자까지의 총합
     let sum = callback2(num);
+
+    // 객체에 담는다.
     let obj = {
         여부 : type,
         합 : sum
     }
+    // 객체를 리턴한다.
     return obj;
 }
 
@@ -18,14 +25,25 @@ function checkType(num){
 }
 
 function getSum(num){
+
+    // 전체 합을 담을 변수
     let sum = 0;
-    for(let i=1; i<num; i++){
-        sum += i;
+
+    // 문을 돌려 각각 모두 더한다.
+    for(let i=0; i<num; i++){
+        sum += i+1;
     }
+
+    // 총합을 리턴한다.
     return sum;
 }
+// 10;
+// let obj = {
+//     여부 : '홀수',
+//     합 : 55
+// }
+console.log(test(10, checkType, getSum).합);
 
-console.log(test(10, checkType, getSum));
 
 
 
